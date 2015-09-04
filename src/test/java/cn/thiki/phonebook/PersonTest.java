fd148ee7a7a3bc90683b1b91d3248838d50069d8
Your Name <email@example.com>
2015-09-04 15:45:24 +0800

    添加Person增删改查测试用例。

--- src/test/java/cn/thiki/phonebook/PersonTest.java
+++ src/test/java/cn/thiki/phonebook/PersonTest.java
@@ -6,8 +6,24 @@ import org.junit.Test;
  * Created by Administrator on 2015/9/4.
  */
 public class PersonTest {
+
     @Test
     public void 查询(){
+
+    }
+
+    @Test
+    public void 删除(){
+
+    }
+
+    @Test
+    public void 插入(){
+
+    }
+
+    @Test
+    public void 修改(){
 
     }
 }
