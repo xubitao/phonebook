506bda7ba987413429b31295d1fd7cc018822311
Your Name <email@example.com>
2015-09-04 15:37:45 +0800

    添加Person查询测试用例。

--- src/test/java/cn/thiki/phonebook/PersonTest.java
+++ src/test/java/cn/thiki/phonebook/PersonTest.java
@@ -1,8 +1,13 @@
 package cn.thiki.phonebook;
 
+import org.junit.Test;
+
 /**
  * Created by Administrator on 2015/9/4.
  */
 public class PersonTest {
+    @Test
+    public void ≤È—Ø(){
 
+    }
 }
