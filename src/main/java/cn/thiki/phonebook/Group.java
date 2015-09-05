3192132dd38fa2f3d5b0646c6ace86e0b5f5adb0
Your Name <email@example.com>
2015-09-05 22:39:48 +0800

    增加分组等类及测试类

---
+++ src/main/java/cn/thiki/phonebook/Group.java
@@ -0,0 +1,7 @@
+package cn.thiki.phonebook;
+
+/**
+ * Created by Administrator on 2015/9/5.
+ */
+public class Group {
+}
---
+++ src/test/java/cn/thiki/phonebook/GroupsTest.java
@@ -0,0 +1,8 @@
+package cn.thiki.phonebook;
+
+/**
+ * Created by Administrator on 2015/9/5.
+ */
+public class GroupsTest {
+
+}
